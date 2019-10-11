5ea8af39b67f20bde024d6dd8a163d5773efaa07
Bladedkiwi <bladedkiw@byui.edu>
2019-10-11 09:10:40 -0500

    commit

--- lesson_4/js/menu.js
+++ lesson_4/js/menu.js
@@ -1,7 +1,10 @@
 function toggleMenu() {
-    var x = document.getElementsByClassName("navigation");
+    var x = document.getElementsByClassName("navigation")[0].className
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-    if (x == x) {
-        document.getElementsByClassName("active")[0].innerHTML = "&#9748; Menu";
+    if (x == "navigation") {
+        document.getElementsByClassName("navigation.responsive")[0].innerHTML = "&#9748;";
+    }
+    else {
+        document.getElementsByClassName("navigation")[0].innerHTML = ("&#9730; Menu");
     }
 }
