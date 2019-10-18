8b13c9c9446894ce9692f17b08e08e91d04cda88
Bladedkiwi <bladedkiw@byui.edu>
2019-10-18 08:25:43 -0500

    commit

--- lesson_5/js/pancake-party.js
+++ lesson_5/js/pancake-party.js
@@ -1,2 +1,2 @@
 var d = new Date();
-var date = (d.getDay() ? 4 : document.querySelector('#pancakes').style.display = "block");
+var date = (d.getDay() ? 5 : document.querySelector('#pancakes').style.display = "block");
