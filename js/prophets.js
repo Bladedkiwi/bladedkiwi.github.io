2eec00a06c86b256b89b7184ef7d74a258f98830
Bladedkiwi <bladedkiw@byui.edu>
2019-11-14 14:40:19 -0600

    js quick change

--- js/prophets.js
+++ js/prophets.js
@@ -5,7 +5,6 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
         //storing the results of the grabbed array
         const prophets = jsonObject['prophets'];
         //looping through each prophet, for the cards
