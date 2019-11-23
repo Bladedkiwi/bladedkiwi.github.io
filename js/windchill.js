c5f8b867c39ee6be036d2cbe02b540d1fe1bbde0
Bladedkiwi <bladedkiw@byui.edu>
2019-11-22 21:52:21 -0600

    minor fixes

--- js/windchill.js
+++ js/windchill.js
@@ -1,5 +1,5 @@
 function getWindChill(tempF,wSpeed) {
-    if (tempF >= 50 && wSpeed <= 3){
+    if (tempF <= 50 && wSpeed > 3){
         var computeWind = 35.74 + (.6215 * tempF) - 35.75 * Math.pow(wSpeed,.16) + (.4275 * tempF * Math.pow(wSpeed,.16));
         computeWind = (Math.floor(computeWind));
         return computeWind;
